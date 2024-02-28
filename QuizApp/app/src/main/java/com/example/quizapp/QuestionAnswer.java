19b53cb2cac63aed98404d8b9f939c7bb36b7b7b
Sanele Mlondi Mkhabela <133631935+Dinzy123@users.noreply.github.com>
2024-02-29 00:08:02 +0200

    Add files via upload

---
+++ QuizApp/app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,33 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String questions[] = {
+        "What is the primary purpose of an internship?",
+        "What is a common goal for interns during their internship?",
+        "How can interns demonstrate initiative during their internship?",
+        "What is the purpose of an internship report?",
+        "What is the importance of networking during an internship?",
+        "What is the difference between an unpaid and a paid internship?"
+
+    };
+
+    public static String choices [][] = {
+            {"To earn a full-time salary", "To gain practical work experience", "To attend lectures and workshops", "To socialize with colleagues"},
+            {"Learning theoretical concepts", "Making long-term career decisions", "Networking with professionals", "All of the above"},
+            {"By completing only assigned tasks", " By asking minimal questions", "By taking on additional responsibilities", "By avoiding collaboration with colleagues"},
+            {"To showcase personal achievements", "To document experiences and learning", "To criticize the organization", "To share personal opinions"},
+            {"It's not important", "It can lead to potential job opportunities", "Networking is only for experienced professionals", "Interns should avoid networking"},
+            {"Unpaid interns don't gain any experience", "Paid interns receive a higher salary", "Unpaid interns receive no compensation, while paid interns receive a salary", "There is no difference"}
+    };
+
+    public static String correctAnswers[] = {
+            "To gain practical work experience",
+            "All of the above",
+            "By taking on additional responsibilities",
+            "To document experiences and learning",
+            "It can lead to potential job opportunities",
+            "Unpaid interns receive no compensation, while paid interns receive a salary"
+    };
+
+}
